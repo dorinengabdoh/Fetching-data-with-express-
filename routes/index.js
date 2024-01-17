5fd0721436a1c5ddd92ad0cdc56a4e39e00e19f1
dorinengabdoh <ngabdohdorine@gmail.com>
2024-01-17 10:29:28 +0100

    fetching data using express

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var axios = require('axios');
-const { BICYCLE_SERVICE_PORT = 4040 } = process.env;
-const bicycleService = 'http://localhost:${BICYCLE_SERVICE_PORT}';
+'use strict'
+const express = require('express')
+const router = express.Router()
+const createError = require('http-errors')
+const { BICYCLE_SERVICE_PORT = 4040, BRAND_SERVICE_PORT = 5050 } = process.env;
 
+const bicycleSrv= `http://localhost:${BICYCLE_SERVICE_PORT}`;
+const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
+router.get('/:id', async function (req, res, next){
+  
+})
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  const { id } = req.params;
-  const { data: bicycle } = await axios.get('${bicycleService}/${id}')
-  res.setHeader("Content-type", "application/json");
-  res.send({
-    id: bicycle.id,
-    color: bicycle.color
-  })
-});
 
 module.exports = router;
